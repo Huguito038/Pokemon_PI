fb72e7f85e81a4ec3a2953778fd3e5ffda12554a
Huguito038 <hugosoler@hotmail.com>
2023-07-01 02:19:52 -0300

    Iniciando PI

---
+++ client/src/Components/Home/Home.jsx
@@ -0,0 +1,13 @@
+import stilos from "../Home/home.module.css"
+
+const Home = ()=>{
+    return (
+        <div className={stilos.hola}>
+            <h3>Hoooooooooola</h3>
+            <h5>Chaaaaaaaaaaaau</h5>
+        </div>
+        
+    )
+}
+
+export default Home;
